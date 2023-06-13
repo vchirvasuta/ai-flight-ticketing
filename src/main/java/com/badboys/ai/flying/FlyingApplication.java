e672e4e40d22d5392c173bad569d630517f73682
valerica_chirvasuta <valerica.chirvasuta@coface..com>
2023-06-13 16:46:36 +0300

    Init repository

---
+++ src/main/java/com/badboys/ai/flying/FlyingApplication.java
@@ -0,0 +1,13 @@
+package com.badboys.ai.flying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/badboys/ai/flying/FlyingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.badboys.ai.flying;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
